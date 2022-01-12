b0b449c862ec082c77cb4bcd13c3cc7f492d620a
Namish295 <81818479+Namish295@users.noreply.github.com>
2022-01-12 20:53:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+prediction_1="";
+prediction_2="";
+prediction_3="";
+
+Webcam.set
+({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:100
+});
+
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log("ml5 version:",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/p4raT8X-W/model.json",modelLoaded);
+
+function modelLoaded()
+{
+    console.log('modelLoaded');
+}
+
+function speak()
+{
+    var synth=window.speechSynthesis;
+    speak_data_1="The first Prediction is"+prediction_1;
+    speak_data_2="The second Prediction is"+prediction_2;
+    speak_data_3="And the third Prediction is"+prediction_3;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2+speak_data_3);
+    synth.speak(utterThis);
+}
